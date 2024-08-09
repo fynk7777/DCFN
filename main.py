91eee8b3c24fd4a2e7b4de8b2172615033b52dbe
fynk7777 bakabonnpapa <fynk7777@yahoo.co.jp>
2024-08-10 06:04:58 +0900

    bump

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import discord
 from discord.ext import commands
 import asyncio
 
-TOKEN = 'YOUR_BOT_TOKEN'  # ここにあなたのBOTトークンを貼り付けます
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True  # 必要なインテントを有効化
