54b642946d3144d87fc5555576113710a25ffb93
fynk7777 bakabonnpapa <fynk7777@yahoo.co.jp>
2024-09-18 02:41:56 +0900

    ステータスをDCFNに設定するようにした

--- main.py
+++ main.py
@@ -36,7 +36,8 @@ async def on_ready():
         print(f'Error syncing commands: {e}')
     check_members.start()
     await send_update_message()
-    await bot.change_presence(activity=discord.Game(name="このサーバー専用BOTです"))
+    await bot.change_presence(status=discord.Status.online, activity=discord.CustomActivity(name=f'DCFN'))
+
 
 async def handle_bump_notification(message):
     master = datetime.now() + timedelta(hours=2)
